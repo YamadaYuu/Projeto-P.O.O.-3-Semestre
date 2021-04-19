ef2bd903716f0ade078c94d37872949f6b9a4190
Yamada Anna <66070734+YamadaYuu@users.noreply.github.com>
2021-04-19 09:29:57 -0300

    edição de cadastro
    Co-Authored-By: Rafael Furtado <49576121+Syank@users.noreply.github.com>

--- projeto-poo/src/main/java/fatec/poo/ProjetoPooApplication.java
+++ projeto-poo/src/main/java/fatec/poo/ProjetoPooApplication.java
@@ -1,13 +1,33 @@
 package fatec.poo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetoPooApplication {
+import fatec.poo.entities.Client;
+import fatec.poo.entitiesRepositorys.ClientRepository;
 
+@SpringBootApplication
+public class ProjetoPooApplication implements CommandLineRunner {
+
+	@Autowired
+	private ClientRepository clientRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoPooApplication.class, args);
 	}
-
+	
+	@Override
+	public void run(String... args) throws Exception {
+		Client cliente = new Client();
+		cliente.setBirthDate("01/01/01");
+		cliente.setEmail("admin@admin.com");
+		cliente.setGender("Feminino");
+		cliente.setName("Adiministrador");
+		cliente.setSenha("1");
+		cliente.setTelefone("123456789");
+		clientRepository.save(cliente);
+	}
+	
 }
